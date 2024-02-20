f15b4e6e60b9a66f9406887369a1d68959b55948
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-02-19 20:13:55 -0500

    completed problems 11 and 12

--- 11_flip_case.py
+++ 11_flip_case.py
@@ -11,3 +11,11 @@ def flip_case(phrase, to_swap):
         'AaaaHHH'
 
     """
+    phrase = list(phrase)
+    for i, char in enumerate(phrase):
+        if char == to_swap.lower():
+            phrase[i] = to_swap.upper()
+        elif char == to_swap.upper():
+            phrase[i] = to_swap.lower()
+
+    return ''.join(phrase)
